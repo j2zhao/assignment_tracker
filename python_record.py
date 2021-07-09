f50c3a6b3cce882c76379ba53e1e92f0c803433a
Jinjin Zhao <jinjinzhao@Jinjins-MacBook-Pro.local>
2021-07-09 16:51:48 -0400

    first commit

---
+++ python_record.py
@@ -0,0 +1,34 @@
+"""
+Python program to replace the compiler
+
+"""
+from git import Repo
+import os
+import time
+
+import sys
+
+import subprocess
+
+COMPILER_COMMAND = 'python' # replace with whatever usually used in the commandline ex. python3
+
+def add_commit(id):
+    """
+    Add current changes and commit
+    """
+    repo = Repo(os.getcwd())
+    repo.git.add('.')
+    repo.git.commit(id)
+
+if __name__ == '__main__':
+    id = str(time.time())
+    add_commit(id + '_start')
+    
+    command = COMPILER_COMMAND + sys.argv[1:]
+
+    process = subprocess.run(command)
+    with open('./runs.txt', 'a') as f:
+        f.write(id + ', error_code: ' + str(process.returncode))
+
+    add_commit(id + '_end')
+    
