d4502c07c13f1759ca7ae33cd8e5ba35a5b45faa
j2zhao <j2zhao@uchicago.edu>
2021-11-05 12:24:41 -0500

    1636133081.319148_start

--- python_record.py
+++ python_record.py
@@ -26,23 +26,26 @@ def check_diff(repo):
         return True
 
 
-def add_commit(id, push = True):
+def add_commit(id, check_changed = True, push = True):
     """
     Add current changes and commit
     """
     # need to check if anything in repo has changed
     repo = Repo(os.getcwd())
-    changed = check_diff(repo) 
-    print(changed)
+    if check_changed:
+        changed = check_diff(repo)
+    else:
+        changed = True
+    
     if changed:
         repo.git.add('.')
         repo.git.commit('-m', id)
         if push:
             repo.remotes.origin.push()
-        return True
+        return changed
     
     else:
-        return False
+        return changed
 
 if __name__ == '__main__':
     id = str(time.time())
@@ -56,5 +59,5 @@ if __name__ == '__main__':
         record = '{} , {}, {} , error_code: {} \n'.format(sys.argv[1], committed, id, process.returncode)
         f.write(record)
 
-    add_commit(id + '_end', push=True)
+    add_commit(id + '_end', check_changed = False, push=True)
     
